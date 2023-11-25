4e1f0fc7efead34d00f7ee6e280f301940533e68
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-11-25 11:56:19 +0530

    update permitives

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// why ts?
-function sum(num1, num2) {
-    return num1 + num2;
+// primitives - number, string, boolean
+function sendSum(num1, num2, name, isWithName) {
+    var result = isWithName ? "Hi ".concat(name, ", Sum is ").concat(num1 + num2) : "Sum is ".concat(num1 + num2);
+    return result;
 }
-var n1 = '10';
-var n2 = 20;
-var value1 = sum(+n1, +n2);
-console.log(value1);
-var value2 = sum(+n1, +n2);
-console.log(value2);
+var num1 = 10;
+var num2 = 10;
+var myName = 'Kavindu';
+var withName = true;
+var result1 = sendSum(10, 20, 'Kavindu', true);
+console.log(result1);
+var result2 = sendSum(10, 20, 'Kavindu', false);
+console.log(result2);
