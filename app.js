70615e7c3ed7ba0b3e3b9b3b46a41f6f32745b39
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-11-25 11:40:57 +0530

    update why ts

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello Typescript!!!");
+// why ts?
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var n1 = '10';
+var n2 = 20;
+var value1 = sum(+n1, +n2);
+console.log(value1);
+var value2 = sum(+n1, +n2);
+console.log(value2);
