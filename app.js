a77a0c9fa86a2c4d3387b6ba85c41e384165641a
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-12-02 11:08:23 +0530

    update heritage

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-// enum
-var Roles;
-(function (Roles) {
-    Roles[Roles["SUPER_ADMIN"] = 0] = "SUPER_ADMIN";
-    Roles[Roles["ADMIN"] = 1] = "ADMIN";
-    Roles[Roles["MANAGER"] = 2] = "MANAGER";
-    Roles[Roles["SELLER"] = 3] = "SELLER";
-    Roles[Roles["BUYER"] = 4] = "BUYER";
-})(Roles || (Roles = {}));
-var myRole = Roles.ADMIN;
-function sendMessage(role) {
-    switch (role) {
-        case Roles.SUPER_ADMIN:
-            console.log("Hello, Super Admin");
-            break;
-        case Roles.ADMIN:
-            console.log("Hello, Admin");
-            break;
-        case Roles.MANAGER:
-            console.log("Hello, Manager");
-            break;
-        case Roles.SELLER:
-            console.log("Hello, Seller");
-            break;
-        default:
-            console.log("Hello, buyer");
+// heritage
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Classification;
+(function (Classification) {
+    Classification["MAMMAL"] = "MAMMAL";
+    Classification["BIRD"] = "BIRD";
+})(Classification || (Classification = {}));
+var Person = /** @class */ (function () {
+    function Person(name, nic, age) {
+        this.classification = Classification.MAMMAL;
+        this.name = name;
+        this.nic = nic;
+        this.age = age;
     }
-}
-sendMessage(Roles.SELLER);
+    return Person;
+}());
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    function Teacher(name, nic, age, subject, college) {
+        var _this = _super.call(this, name, nic, age) || this;
+        _this.subject = subject;
+        _this.college = college;
+        return _this;
+    }
+    Teacher.prototype.getNic = function () {
+        return this.nic;
+    };
+    return Teacher;
+}(Person));
+var teacher = new Teacher("Kavindu", "1232312321", 28, "CSE", "IJSE");
+console.log(teacher.name);
+console.log(teacher.getNic());
+console.log(teacher.age);
+console.log(teacher.subject);
+console.log(teacher.college);
+console.log(teacher.classification);
