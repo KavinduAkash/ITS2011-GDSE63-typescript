cc0ad2caae9918b6b7aae8ada635360b7d7b44c4
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-12-02 10:38:18 +0530

    update enums

--- app.js
+++ app.js
@@ -1,61 +1,29 @@
-var owner_name_input_element = document.getElementById('ownerName');
-var fixed_amount_input_element = document.getElementById('fixedAmount');
-var add_fix_btn_element = document.getElementById('addFixBtn');
-var tbl_body_element = document.getElementById('fixedDepositTableBody');
-var update_interest_rate_btn_element = document.getElementById('updateInterestBtn');
-var new_interest_rate_input_element = document.getElementById('newInterestRate');
-/*
-* owner_name
-* fixed_amount
-* interest_rate
-* annual_interest
-* */
-var Account = /** @class */ (function () {
-    function Account(owner_name, fixed_amount, rate, annual_interest) {
-        this.owner_name = owner_name;
-        this.fixed_amount = fixed_amount;
-        this.interest_rate = rate;
-        this.annual_interest = annual_interest;
+// enum
+var Roles;
+(function (Roles) {
+    Roles[Roles["SUPER_ADMIN"] = 0] = "SUPER_ADMIN";
+    Roles[Roles["ADMIN"] = 1] = "ADMIN";
+    Roles[Roles["MANAGER"] = 2] = "MANAGER";
+    Roles[Roles["SELLER"] = 3] = "SELLER";
+    Roles[Roles["BUYER"] = 4] = "BUYER";
+})(Roles || (Roles = {}));
+var myRole = Roles.ADMIN;
+function sendMessage(role) {
+    switch (role) {
+        case Roles.SUPER_ADMIN:
+            console.log("Hello, Super Admin");
+            break;
+        case Roles.ADMIN:
+            console.log("Hello, Admin");
+            break;
+        case Roles.MANAGER:
+            console.log("Hello, Manager");
+            break;
+        case Roles.SELLER:
+            console.log("Hello, Seller");
+            break;
+        default:
+            console.log("Hello, buyer");
     }
-    Account.prototype.updateRate = function (rate) {
-        this.interest_rate = rate;
-        this.annual_interest = (this.fixed_amount * rate) / 100;
-    };
-    Account.prototype.getInterestRate = function () {
-        return this.interest_rate;
-    };
-    Account.prototype.getAnnualInterest = function () {
-        return this.annual_interest;
-    };
-    return Account;
-}());
-var rate = 12.5;
-var accounts = [];
-add_fix_btn_element.addEventListener('click', function () {
-    var owner_name = owner_name_input_element.value;
-    var fixed_amount = fixed_amount_input_element.value;
-    var annual_interest = (+fixed_amount * rate) / 100;
-    var account = new Account(owner_name, +fixed_amount, rate, annual_interest);
-    accounts.push(account);
-    updateTbl();
-});
-function updateTbl() {
-    tbl_body_element.innerHTML = "";
-    accounts.map(function (r) {
-        var record_element = document.createElement('tr');
-        var record_data = "<td>".concat(r.owner_name, "</td>\n                        <td>").concat(r.fixed_amount, "</td>\n                        <td>").concat(r.getInterestRate(), "</td>\n                        <td>").concat(r.getAnnualInterest(), "</td>");
-        record_element.innerHTML = record_data;
-        tbl_body_element.appendChild(record_element);
-    });
 }
-update_interest_rate_btn_element.addEventListener('click', function () {
-    var new_interest_rate = new_interest_rate_input_element.value;
-    rate = +new_interest_rate;
-    var newAccounts = [];
-    accounts.map(function (r) {
-        r.updateRate(rate);
-        newAccounts.push(r);
-    });
-    accounts = newAccounts;
-    updateTbl();
-});
+sendMessage(Roles.SELLER);
