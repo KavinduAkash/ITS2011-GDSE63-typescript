e436be9c8e818e980a8a4d798a71ddce53b61350
KavinduAkash <bkkasamarasingha1220@gmail.com>
2023-12-02 09:03:45 +0530

    update ex bank - create Account class

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-var Person = /** @class */ (function () {
-    function Person(name, age, nic) {
-        this.name = name;
-        this.age = age;
-        this.nic = nic;
-    }
-    Person.prototype.getNic = function () {
-        return this.nic;
-    };
-    return Person;
-}());
-var person1 = new Person('Kavindu', 28, '12312312321');
-console.log(person1.name);
-// console.log(person1.nic); // can't access nic directly because nic is private attribute
-console.log(person1.getNic());
-var DBConnection = /** @class */ (function () {
-    function DBConnection(value) {
-        DBConnection.connection = value;
-    }
-    DBConnection.getConnection = function () {
-        if (!DBConnection.connection) {
-            new DBConnection('test-connection');
-        }
-        return DBConnection.connection;
-    };
-    return DBConnection;
-}());
-var connection = DBConnection.getConnection();
-console.log('Connection: ', connection);
+var owner_name_input_element = document.getElementById('ownerName');
+var fixed_amount_input_element = document.getElementById('fixedAmount');
+var add_fix_btn_element = document.getElementById('addFixBtn');
+add_fix_btn_element.addEventListener('click', function () {
+    var owner_name = owner_name_input_element.value;
+    var fixed_amount = fixed_amount_input_element.value;
+    console.log("owner= ".concat(owner_name, ", fixed amount= ").concat(fixed_amount));
+});
--- app.ts
+++ app.ts
@@ -1,3 +1,33 @@
-// tuple - A tuple is a collection which is ordered and unchangeable.
+let owner_name_input_element = document.getElementById('ownerName') as HTMLInputElement;
+let fixed_amount_input_element = document.getElementById('fixedAmount') as HTMLInputElement;
+let add_fix_btn_element = document.getElementById('addFixBtn');
 
-let data: [string, number, string, boolean, number] = ["asdas", 1, "dads", true, 2];
+/*
+* owner_name
+* fixed_amount
+* interest_rate
+* annual_interest
+* */
+
+class Account {
+  owner_name: string;
+  fixed_amount: number;
+  interest_rate: number;
+  annual_interest: number;
+
+  constructor(owner_name: string, fixed_amount: number, rate: number, annual_interest: number) {
+    this.owner_name = owner_name;
+    this.fixed_amount = fixed_amount;
+    this.interest_rate = rate;
+    this.annual_interest = annual_interest;
+  }
+}
+
+add_fix_btn_element.addEventListener('click', () => {
+
+  let owner_name = owner_name_input_element.value;
+  let fixed_amount = fixed_amount_input_element.value;
+
+  console.log(`owner= ${owner_name}, fixed amount= ${fixed_amount}`);
+
+});
